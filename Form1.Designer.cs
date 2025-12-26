53fe85d8541aa2a1bfdf53a3c640f3c695ea67d9
Thanh Nguyen VN <minhthanhitx93@gmail.com>
2025-12-26 18:57:34 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace frmactivenotify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.btnexcute = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbltitle);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblStatus);
+            this.groupBox2.Controls.Add(this.btnexcute);
+            this.groupBox2.Location = new System.Drawing.Point(0, 71);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(409, 99);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.Location = new System.Drawing.Point(0, 32);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(409, 18);
+            this.lbltitle.TabIndex = 0;
+            this.lbltitle.Text = "label1";
+            this.lbltitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnexcute
+            // 
+            this.btnexcute.Location = new System.Drawing.Point(159, 42);
+            this.btnexcute.Name = "btnexcute";
+            this.btnexcute.Size = new System.Drawing.Size(75, 23);
+            this.btnexcute.TabIndex = 0;
+            this.btnexcute.Text = "DO";
+            this.btnexcute.UseVisualStyleBackColor = true;
+            this.btnexcute.Click += new System.EventHandler(this.btnexcute_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Gray;
+            this.lblStatus.Location = new System.Drawing.Point(12, 79);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(61, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Checking...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 172);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(426, 211);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(426, 211);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbltitle;
+        private System.Windows.Forms.Button btnexcute;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Windows.Forms;
+
+namespace frmactivenotify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //lấy thời gian hiện tại theo hệ thống
+            DateTime gioht = DateTime.Now;
+            //hiển thị thời gian lên thanh tiêu đề của form
+            this.Text = gioht.ToString();
+
+        }
+        private void CheckStatus()
+        {
+            string registryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System";
+            string valueName = "VerboseStatus";
+
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryPath, false))
+                {
+                    // Kiểm tra xem Key và Giá trị có tồn tại không
+                    object val = (key != null) ? key.GetValue(valueName) : null;
+
+                    if (val != null && Convert.ToInt32(val) == 1)
+                    {
+                        lblStatus.Text = "Status: ENABLED (ON)";
+                        lblStatus.ForeColor = Color.Green;
+                        btnexcute.Text = "Disable Verbose"; // Đổi text nút luôn cho tiện
+                    }
+                    else
+                    {
+                        lblStatus.Text = "Status: DISABLED (OFF)";
+                        lblStatus.ForeColor = Color.Red;
+                        btnexcute.Text = "Enable Verbose";
+                    }
+                }
+            }
+            catch
+            {
+                lblStatus.Text = "Status: Unknown (Need Admin)";
+                lblStatus.ForeColor = Color.Gray;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckStatus();  
+            //tiêu đề của form khi load ở label1
+            lbltitle.Text= "VerboseTool - Active Detail Notify";
+        }
+
+        private void btnexcute_Click(object sender, EventArgs e)
+        {
+            string registryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System";
+            string valueName = "VerboseStatus";
+
+            try
+            {
+                // 1. Mở key với quyền ghi (true)
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registryPath, true))
+                {
+                    if (key != null)
+                    {
+                        // 2. Kiểm tra trạng thái hiện tại từ Registry
+                        object val = key.GetValue(valueName);
+                        int currentState = (val != null) ? Convert.ToInt32(val) : 0;
+
+                        // 3. Đảo trạng thái (Nếu đang 1 thì về 0, nếu đang 0 thì lên 1)
+                        int newState = (currentState == 1) ? 0 : 1;
+                        key.SetValue(valueName, newState, RegistryValueKind.DWord);
+
+                        // 4. Thông báo cho người dùng
+                        string msg = (newState == 1)
+                            ? "Verbose notification mode is ENABLED!"
+                            : "Verbose notification mode is DISABLED!";
+
+                        MessageBox.Show(msg + "\n\nPlease restart your computer to apply changes.",
+                                        "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // 5. Cập nhật lại giao diện (màu sắc, chữ) ngay lập tức
+                        CheckStatus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error: System Registry path not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Error: Administrator privileges are required to repair the system!", "Permissions", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
